fb0ca08e6bcef8e311974a84b77bd5a95a9ded97
Yoidea <yoidea.cc@gmail.com>
2019-01-06 18:23:06 +0900

    the Context API is running!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, createContext } from 'react';
 import './App.css';
+import { createStore } from 'redux';
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+const { Provider, Consumer } = createContext();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.state = {
+      number: 0,
+      count: 0,
+      increment: this.increment
+    };
+    store.subscribe(() => {
+      this.setState({ number: store.getState() });
+    });
+  }
+
+  increment() {
+    this.setState({count: this.state.count + 1});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Provider value={this.state}>
+            <Consumer>
+              {
+                ({ count, increment }) =>  {
+                  return (
+                    <div>
+                      <h2>{count}</h2>
+                      <button onClick={increment}>INCREMENT</button>
+                    </div>
+                  );
+                }
+              }
+            </Consumer>
+          </Provider>
+          <h2>{this.state.number}</h2>
+          <button onClick={() => {
+            store.dispatch({ type: 'INCREMENT' });
+          }}>INCREMENT</button>
         </header>
       </div>
     );
